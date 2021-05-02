b55fb003efedf002f192ec5e3fde16c38c548640
sanika <sanikaraul06@gmail.com>
2021-05-02 12:09:00 +0530

    First Commit

---
+++ jtwo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class jtwo {
+
+    public static void main(String[] args) {
+
+        
+     
+        System.out.println("You entered into job2 " );
+    }
+}
